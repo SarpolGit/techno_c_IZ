b691d7cfb91009bbde0291b2788fc44ae4e1b66c
Shirshov Artyom <artyomsh01@yandex.ru>
2021-03-13 20:57:04 +0300

    Fixed CMake Version

--- tests.cpp
+++ tests.cpp
@@ -19,6 +19,16 @@ TEST (translate_to_roman, negative_int) {
     ASSERT_EQ(0, len);
 }
 
+TEST (is_positive, second_test){
+    int r = is_positive(100);
+    ASSERT_EQ(100, r);
+}
+
+TEST (is_positive, negative_int){
+    int r = is_positive(-233);
+    ASSERT_EQ(-1, r);
+}
+
 int main(int argc, char** argv){
 
     ::testing::InitGoogleTest(&argc, argv);
